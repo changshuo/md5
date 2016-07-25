8d7434ba0ff7490df6e9fa42ba26df307dbf6a46
Shuo Chang <shuocha@microsoft.com>
2016-07-25 12:13:29 -0700

    modularize md5 lib

---
+++ README.md
@@ -0,0 +1,3 @@
+Compiling with Visual C++ 2015 Update 1 or after:
+
+cl /c /experimental:module md5.ixx
--- md5.h
+++ md5.h
@@ -23,23 +23,25 @@
  * See md5.c for more information.
  */
 
+module md5;
+
 #ifdef HAVE_OPENSSL
 #include <openssl/md5.h>
 #elif !defined(_MD5_H)
 #define _MD5_H
 
 /* Any 32-bit or wider unsigned integer data type will do */
-typedef unsigned int MD5_u32plus;
+export typedef unsigned int MD5_u32plus;
 
-typedef struct {
+export typedef struct {
 	MD5_u32plus lo, hi;
 	MD5_u32plus a, b, c, d;
 	unsigned char buffer[64];
 	MD5_u32plus block[16];
 } MD5_CTX;
 
-extern void MD5_Init(MD5_CTX *ctx);
-extern void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
-extern void MD5_Final(unsigned char *result, MD5_CTX *ctx);
+export void MD5_Init(MD5_CTX *ctx);
+export void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
+export void MD5_Final(unsigned char *result, MD5_CTX *ctx);
 
 #endif
